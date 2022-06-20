4a30d5b6ce6832738d571244947ab576379c4569
Paul Ludwigs <paul.ludwigs@web.de>
2022-06-20 14:08:16 +0200

    create navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div class="container">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+  </nav>
+</div>
+  )
+}
+
+export default Navbar
