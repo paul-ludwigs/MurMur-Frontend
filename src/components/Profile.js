213595b6da0ce1719403c2afd9c8169a2d002c7c
Mehtap <kayhanmehtap10@gmail.com>
2022-06-21 15:26:10 +0200

    Create a profile componente, edit and upload button

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import axios from "axios";
 function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <>
+      <div class="card-body text-center mt-5">
+        <img
+          src={require("../images/profilepic.JPG")}
+          alt="profilepic"
+          className="rounded-circle img-fluid"
+          style={{ width: "150px" }}
+        />
+        <div className="container mx-auto text-center mt-5">
+          <label for="file-upload" class="custom-file-upload">
+            Add picture
+          </label>
+          <input id="file-upload" type="file" />
+        </div>
+
+        <h1 className="my-3">Username</h1>
+        <h2 className="my-3">Username@email.com</h2>
+        <button type="button" class="btn">
+          Edit profile
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default Profile
+export default Profile;
