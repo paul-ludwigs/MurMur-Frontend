1115c95d176d46b64e6b8ce5d53a18e50f1c57f9
Philipp Strauß <phil13.strauss@gmail.com>
2022-06-21 15:32:19 +0200

    styling buttons+margins

--- src/components/Create.js
+++ src/components/Create.js
@@ -9,7 +9,7 @@ function Create() {
      </div>
       <form>
 
-        <div className="text-center mb-3">
+        <div className="text-center container mb-3 mx-auto">
           <label htmlFor="exampleFormControlInput1" className="form-label">
             City
           </label>
@@ -17,24 +17,24 @@ function Create() {
         </div>
 
         <div className="container mx-auto text-center mt-3">          
-          <button type="button" className="btn btn-sm btn-primary mt-2 mx-1 active" data-bs-toggle="button" autocomplete="off" aria-pressed="true">
+          <button type="button" className="crbutton btn btn-sm btn-primary mt-2 mx-1 active" data-bs-toggle="button" autocomplete="off" aria-pressed="true">
             Food+Drink
           </button>
-          <button type="button" className="btn btn-sm btn-primary mt-2 mx-1" data-bs-toggle="button" autocomplete="off">
+          <button type="button" className="crbutton btn btn-sm btn-primary mt-2 mx-1" data-bs-toggle="button" autocomplete="off">
             Location
           </button>
-          <button type="button" className="btn btn-sm btn-primary mt-2 mx-1" data-bs-toggle="button" autocomplete="off">
+          <button type="button" className="crbutton btn btn-sm btn-primary mt-2 mx-1" data-bs-toggle="button" autocomplete="off">
             Sport+Activity
           </button>
         </div>
         <div className="container mx-auto text-center mt-1  mb-2">          
-          <button type="button" className="btn btn-sm btn-primary mt-2 mx-1 active" data-bs-toggle="button" autocomplete="off" aria-pressed="true">
+          <button type="button" className="crbutton btn btn-sm btn-primary mt-2 mx-1 active" data-bs-toggle="button" autocomplete="off" aria-pressed="true">
             Event
           </button>
-          <button type="button" className="btn btn-sm btn-primary mt-2 mx-1" data-bs-toggle="button" autocomplete="off">
+          <button type="button" className="crbutton btn btn-sm btn-primary mt-2 mx-1" data-bs-toggle="button" autocomplete="off">
             General
           </button>
-          <button type="button" className="btn btn-sm btn-primary mt-2 mx-1" data-bs-toggle="button" autocomplete="off">
+          <button type="button" className="crbutton btn btn-sm btn-primary mt-2 mx-1" data-bs-toggle="button" autocomplete="off">
             Warning
           </button>
         </div>
@@ -42,7 +42,7 @@ function Create() {
 
 
 
-        <div className="text-center mb-3">
+        <div className="text-center mb-3 mt-3 container mx-auto">
           <label htmlFor="exampleFormControlTextarea1" className="form-label">
             You can enter your Tip below
           </label>
@@ -50,7 +50,7 @@ function Create() {
         </div>
 
         <div className="text-center">
-        <button type="submit" className="btn btn-info mb-3 mt-5">
+        <button type="submit" className="btn btn-info mb-3 mt-3">
           Submit
         </button>
       </div>
