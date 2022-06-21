b48214b0bcfa190e4c494f17962d1a5e481b6606
Mehtap <kayhanmehtap10@gmail.com>
2022-06-21 16:52:31 +0200

    change the attribute name class into className

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,14 @@
 import React from "react";
 import axios from "axios";
-import {useState} from "react";
+import { useState } from "react";
 function Profile() {
-
-  const [userName, setUserName] = useState("Username");
+  const [userName, setUserName] = useState("Max");
   const [eMail, setEMail] = useState("Username@email.com");
 
   const clickHandler = () => {
-// über setUserName & serEMail können die Daten geändert werden
-  }
+    // put request 
+    // über setUserName & serEMail können die Daten geändert werden
+  };
   return (
     <>
       <div className="card-body text-center mt-5">
@@ -19,17 +19,20 @@ function Profile() {
           style={{ width: "150px" }}
         />
         <div className="container mx-auto text-center mt-5">
-          <label htmlFor="file-upload" class="custom-file-upload">
+          <label htmlFor="file-upload" className="custom-file-upload">
             Add picture
           </label>
           <input id="file-upload" type="file" />
         </div>
-
-        <h1 className="my-3">{userName}</h1>
-        <h2 className="my-3">{eMail}</h2>
-        <button type="button" className="btn" onClick={clickHandler}>
-          Edit profile
-        </button>
+        <form>
+          <input className="my-3" value={userName} />
+          <br />
+          <input className="my-3" value={eMail} />
+          <br />
+          <button type="button" className="btn" onClick={clickHandler}>
+            Edit profile
+          </button>
+        </form>
       </div>
     </>
   );
