35db058a8990b0a83148dc30b65bc39df38576c6
Mehtap <kayhanmehtap10@gmail.com>
2022-06-29 11:23:03 +0200

    change text on landingpage

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -1,97 +1,94 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Landingpage = () => {
   const api = process.env.REACT_APP_API_URL;
-  const { isAuthenticated, setIsAuthenticated } =
-    useContext(AuthContext);
+  const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-
-      const email = e.target[0].value;
-      const password = e.target[1].value;
-      const loginData = {
-       email: email,
-       password: password,
-      };
-      if(!email || !password){
-        toast("Please fill out all forms!")
-      } else {
-          try {
-            const res = await axios.post(
-              `${api}/users/login`,
-              loginData
-            );
-            console.log(res)
-            const token = res.headers.authorization;        
-            localStorage.setItem("token", token);
-            setIsAuthenticated(true);
-          } catch (error) {
-            console.log(error);
-            toast("Unable to login. Please check your login credentials!")
-          }
-         }
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+    const loginData = {
+      email: email,
+      password: password,
     };
+    if (!email || !password) {
+      toast("Please fill out all forms!");
+    } else {
+      try {
+        const res = await axios.post(`${api}/users/login`, loginData);
+        console.log(res);
+        const token = res.headers.authorization;
+        localStorage.setItem("token", token);
+        setIsAuthenticated(true);
+      } catch (error) {
+        console.log(error);
+        toast("Unable to login. Please check your login credentials!");
+      }
+    }
+  };
 
-    if (isAuthenticated === true) return <Navigate to="../search" />;
+  if (isAuthenticated === true) return <Navigate to="../search" />;
 
   return (
-
     <>
-    <ToastContainer />
-      <div className="container mx-auto text-center mt-5">
-        <p className="card-text">
-          Our app gives you tips but also warnings from other travelers. But you
-          are also welcome to share your experiences with others.
+      <ToastContainer />
+      <div className="container mx-auto text-center mt-3">
+        <p className="card-text mt-" style={{padding:"0.5em"}}>
+          You don't know where to go yet or you are already at your destination
+          and want to discover more? No problem! Download our app and learn
+          useful tips as well as warnings from other travelers. But you are
+          also welcome to share your experiences with others.
         </p>
 
         <img
           src={require("../images/landingpage_image.JPG")}
           className="card-img-top image align-self-center img-thumbnail"
           alt="landingpageimage"
+          style={{padding:"0.5em"}}
         />
 
         <div className="card-body">
-          <p className="card-text">
-            Let's create an account to create a travel tip or planning your
+          <p className="card-text" style={{padding:"2rem"}}>
+            Let's login to create a travel tip or planning your
             trip!
           </p>
         </div>
 
         {/* Form */}
         <form onSubmit={handleSubmit} id="loginformular">
-        <div className="mb-3 row mt-3">
-          <label htmlFor="staticEmail" className="col-sm-2 col-form-label">
-            Email
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="email"
-              className="form-control"
-              id="staticEmail"
-              placeholder="email@example.com"
-            />
+          <div className="mb-3 row mt-3">
+            <label htmlFor="staticEmail" className="col-sm-2 col-form-label">
+              Email
+            </label>
+            <div className="col-sm-10">
+              <input
+                type="email"
+                className="form-control"
+                id="staticEmail"
+                placeholder="email@example.com"
+              />
+            </div>
           </div>
-        </div>
-        <div className="mb-3 row">
-          <label htmlFor="inputPassword" className="col-sm-2 col-form-label">
-            Password
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="password"
-              className="form-control"
-              id="inputPassword"
-            />
+          <div className="mb-3 row">
+            <label htmlFor="inputPassword" className="col-sm-2 col-form-label">
+              Password
+            </label>
+            <div className="col-sm-10">
+              <input
+                type="password"
+                className="form-control"
+                id="inputPassword"
+              />
+            </div>
           </div>
-        </div>
           <button type="submit" className="button btn btn-info mb-3 mt-5">
             Sign-In
           </button>
@@ -99,9 +96,7 @@ const Landingpage = () => {
 
         {/* Button/Link */}
         <div className="text-center">
-          <Link to="/register">
-              Register
-          </Link>         
+          <Link to="/register">Register</Link>
         </div>
       </div>
     </>
