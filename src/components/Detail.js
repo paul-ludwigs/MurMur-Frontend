f9858b7928a46790c68f97d86f1593951fe45295
Mehtap <kayhanmehtap10@gmail.com>
2022-07-04 13:43:47 +0200

    style detailpage, fix image position bug

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -138,13 +138,11 @@ function Detail() {
                   {murmur.city.charAt(0).toUpperCase() + murmur.city.slice(1)}
                 </h1>
               </div>
-              <div className="transparent-bg"> 
-              <div className="container-md  mx-3 my-3">
-                <div className="row">
-                  <div className="col-sm-12"></div>
-                </div>
+              <div className="transparent-bg text-center"> 
+              <div className="container" >
+                
 
-                <div className="col-0 col-sm-0">
+                <div className="col-12 col-sm-12">
                   <div className="row justify-content-center sm-4">
                     <img
                       src={
